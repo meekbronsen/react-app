5a6fae019bd112838429135f3bd6271dc3e9f96e
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-08-19 22:23:26 +0300

    CSS-IN-JS.

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,18 @@
 import { useState } from "react";
 import styles from './ListGroup.module.css'
+import styled from 'styled-components'
+
+const List = styled.ul` 
+  list-style: none;
+  `
+  
+const ListItem = styled.li<ListItemProps> /* Defining the shape of the prop */` 
+  padding: 5px 0;
+  background: ${props => props.active ? 'blue' : 'none'}
+`
+interface ListItemProps{
+  active: boolean;
+}
 
 interface ListGroupProps {
   items: string[];
@@ -14,21 +27,21 @@ function ListGroup({items, heading, onSelectItem} : ListGroupProps) {
   return (
     <>
       <h1>{heading}</h1>
-      {/* <ul className={styles.listGroup}> */}
-      <ul className={[styles.listGroup, styles.container].join(' ')}>
+      <List className={[styles.listGroup, styles.container].join(' ')}>
         {items.map((item, index) => (
-          <li 
+          <ListItem
+          active={index === selectedIndex} 
           key={item} 
           className={ selectedIndex === index ? "list-group-item active" : "list-group-item"}
-          onClick={() => { onSelectItem(item) 
+          onClick={() => { onSelectItem(item)  
             setSelectedIndex(index)
           }} 
           >
             {" "}
             {item}{" "}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
