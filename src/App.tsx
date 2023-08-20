34df1977f73e90d385f8451ef78f3fe9d45a155d
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-08-20 22:40:16 +0300

    State management: Updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import Like from "./components/Like/Like";
+import {useState} from 'react'
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5
+  })
 
-  const eventLike = () => console.log('liked')
-  const eventUnlike = () => console.log('unliked')
+// copying the object as an update
+// const handleClick = () => {
+//   const newDrink = {
+//     title: drink.title,
+//     price: 6
+//   }
+//   setDrink(newDrink)
+// }
 
   return (
     <div>
-      <Like like={eventLike} unlike={eventUnlike} ></Like>
+      {drink.price}
+      {/* <button onClick={handleClick}> My Button </button> */}
+      {/* using the spread operator if the object being copied is extensive */}
+      <button onClick={ () =>setDrink({...drink, price: 7})}> </button>
     </div>
   );
 }
