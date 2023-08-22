1e8df6cb246015e7da4b4ed8d12dfd4dff3181ad
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-08-23 00:56:23 +0300

    Accesing Input Fields
    Collecting data from the input fields on submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,35 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
-    // if code in onSubmit gets complex, we can create a handler to hold all
+    // useRef is another hook in react like useState. it can be used to reference a DOM element.
+    // we gonna to use it to read value in a form on submission.
+    
+    // nameRef has a single property called current that returns the DOM element we are referencing
+    const nameRef = useRef<HTMLInputElement>(null); // <HTMLInputElement> to clear the value error by showing typescript compiler which element we're referencing
+    const ageRef =  useRef<HTMLInputElement>(null);
+    const person = { name: '', age: 0}
+
     const handler = (event: FormEvent) => {
-        event.preventDefault()  // calling event object's preventDefault to prevent default behaviour
-        console.log('submitted')
+        event.preventDefault()  
+        if (nameRef.current !== null)
+            // console.log(nameRef.current.value) // value refers to the data inside the input field
+            person.name = nameRef.current.value
+        if (ageRef.current !== null)
+            person.age= parseInt(ageRef.current.value) // parseInt is used to convert string into a number
+        console.log(person)
     }
 
     return(
     <form onSubmit={handler}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">username</label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
-      <button type="submit" className="btn btn-primary"> Submit </button>
+      <button  type="submit" className="btn btn-primary"> Submit </button>
     </form>
   );
 };
