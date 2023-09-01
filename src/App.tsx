00979643bf54a6aaaab8e4c7566103f0d155287a
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-09-02 01:06:46 +0300

    Deleting Data

--- src/App.tsx
+++ src/App.tsx
@@ -27,19 +27,50 @@ function App() {
         if (err instanceof CanceledError) return;
         setError(err.message);
         setLoading(false);
-      })
-      // The finally method will be executed when the promise is settled (resolved or rejected). It is just here to avoid duplication of setLoading state
-      // .finally(() => {setLoading(false)});
-
+      });
+    // The finally method will be executed when the promise is settled (resolved or rejected). It is just here to avoid duplication of setLoading state
+    // .finally(() => {setLoading(false)});
 
     return () => controller.abort();
   }, []);
+
+  // Optimistic Update
+  const deleteUser = (user: User) => {
+      const originalUsers = [...users]
+
+    // Updating the ui
+    setUsers(users.filter((i) => i.id !== user.id));
+    // Calling the server to save the changes
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        // If we catch an error, we display err.message and reset to original users list
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
-      { isLoading && <div className="spinner-border"></div>}
+      {isLoading && <div className="spinner-border"></div>}
       {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
+        <ul className="list-group">
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {/* the deleteUserfunction */}
+            <p>{user.id}</p>
+            {user.name}
+            <button
+              onClick={() => deleteUser(user)}
+              className="btn btn-outline-danger"
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
       ))}
     </>
   );
