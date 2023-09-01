6257403a2f03a62ea3029dba7f6caa4e5f045e6c
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-09-01 23:02:15 +0300

    Showing loading Indicator.
    Loading Indicator will appear as data is being fetched

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,39 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    // A clean up function to cancel the fetch request incase the data is no longer needed
-    const controller =  new AbortController()  // A new builtin class that allows to cancel or abort asynchronous operations ( DOM operations, fetch requests)
-    
+    // A clean up function to cancel fetch requests incase the data is no longer needed
+    const controller = new AbortController(); // A new builtin class that allows to cancel or abort asynchronous operations ( DOM operations, fetch requests)
+
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {signal: controller.signal} )// the second argument is a request config object
-      .then((response) => setUsers(response.data))
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      }) // the second argument is a request config object
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)
-      });
+        setError(err.message);
+        setLoading(false);
+      })
+      // The finally method will be executed when the promise is settled (resolved or rejected). It is just here to avoid duplication of setLoading state
+      // .finally(() => {setLoading(false)});
 
-    return () => controller.abort()
+
+    return () => controller.abort();
   }, []);
   return (
-    <div>
+    <>
       {error && <p className="text-danger">{error}</p>}
+      { isLoading && <div className="spinner-border"></div>}
       {users.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
-    </div>
+    </>
   );
 }
 
