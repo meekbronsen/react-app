3340899a90bbe0d0cfc42bdd11dd5e463b2dbc6f
meekbronsen <mikolskeezy@gmail.com>
2023-09-01 17:00:20 +0300

    Working with async and await.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-// As good developers we should anticipate errors and handle them properly
-// Handling of errors generated from server requests
-
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -11,18 +8,25 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((response) => setUsers(response.data))
-      // adding the catch method to catch errors
-      .catch((err) => setError(err.message));
+    // adding a try catch block to catch errors
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get<User[]>( // we can put await infront of the promise to extract the response data
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(response.data);
+      } catch (err) {
+        setError((err as AxiosError).message); // If there is an error in the promise error message will be changed
+      }
+    };
+    fetchUsers(); // we then call it
   }, []);
   return (
     <div>
-      { error && <p className="text-danger">{error}</p>}
+      {error && <p className="text-danger">{error}</p>}
       {users.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
