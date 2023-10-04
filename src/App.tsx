b83523d0a983522c9892620966bda59be5c84e5c
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-10-04 22:56:32 +0300

    Creating complex actions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import TodoList from "./react-query/TodoList";
 import PostList from "./react-query/PostList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
 
   return (
-    <Counter></Counter>
+    <TaskList></TaskList>
   );
 }
 
