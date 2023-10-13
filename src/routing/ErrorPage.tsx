f33f3997345ca082018c09774a45bc60a2253020
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-10-14 00:49:00 +0300

    Handling Errors

---
+++ src/routing/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  // Now since react-router catches all types errors, it doesn't matter if it's routing error or error from the application,
+  // the errorElement set will be displayed for every error. But we wan to differentiate errors from app or error from routing.
+
+  const error = useRouteError(); // This hook catches errors
+  const isRouteError = isRouteErrorResponse(error); 
+  return (
+    <>
+      <h1>Oops...</h1>
+      <p>
+        {isRouteError? 'Invalid url' : 'Unexpected error'}
+      </p>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,12 +3,14 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import UserDetailPage from "./UserDetailPage";
 import Layout from "./Layout";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
     // We move all the dynamic routes inside children array
     {
         path: '/',
         element: <Layout/>,
+        errorElement: <ErrorPage/>, // the error element will be displayed in Layout element
         children: [
             {path: '', element: <HomePage/>},
             {path: 'users', element: <UserListPage/>},
