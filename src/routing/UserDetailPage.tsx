76105ebadf6e8bd81ff5d9c3c5e5f83441688d7d
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-10-13 23:56:30 +0300

    Getting Data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,16 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  // useParams extracts parameters from the URL
+  const param = useParams();
+
+  // useSearchParams can be used to acces and update the querystring parameters. PLEASE NOTE: querystring parameters
+  // setSearchParams function has a sideeffect where it is making updates outside the component, so call it only inside eventhandlers and useEffect()
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log(searchParams.get('name'));
+
+  const location = useLocation(); // This Hook shows details
+  
   return <p>User</p>;
 };
 
